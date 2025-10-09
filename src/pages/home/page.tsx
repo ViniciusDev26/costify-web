60a322fd1be72fbedf5ed7ca47a22a5b3ee9345c
Francisco Sales <saneto18@hotmail.com>
2025-10-09 19:58:16 -0300

    Botão para gestão de ingredientes

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,15 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 import { Switch } from "../../components/ui/switch";
 import { useThemeStore } from "../../stores/theme/store";
 
 export function Home() {
-	const { theme, toggleTheme } = useThemeStore();
+  const { theme, toggleTheme } = useThemeStore();
 
-	return (
-		<div className="w-full h-screen flex items-center justify-center">
-			<div className="flex-col gap-4">
-				<p>COSTIFY</p>
-				<Switch checked={theme === "DARK"} onClick={toggleTheme} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="flex-col gap-4">
+        <p>COSTIFY</p>
+        <Switch checked={theme === "DARK"} onClick={toggleTheme} />
+      </div>
+    </div>
+  );
+}
+
+export default function HomePage() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-6">
+      <Home />
+      <Link to="/ingredients">
+        <Button>
+          Gerenciar Ingredientes
+        </Button>
+      </Link>
+    </div>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router";
-import { Home } from "@/pages/home/page";
+import HomePage from "@/pages/home/page";
 import { IngredientsPage } from "@/pages/ingredients/page";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
-		Component: Home,
+		Component: HomePage
 	},
 	{
 		path: "/ingredients",
